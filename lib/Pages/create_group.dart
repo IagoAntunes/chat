6a859f27218469df8dda3b3a0e798bb9b610f0db
IagoAntunes <iagoantunes.f@gmail.com>
2022-10-23 12:13:35 -0300

    pages

---
+++ lib/Pages/create_group.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CreateRoomPage extends StatefulWidget {
+  const CreateRoomPage({super.key});
+
+  @override
+  State<CreateRoomPage> createState() => _CreateRoomPageState();
+}
+
+class _CreateRoomPageState extends State<CreateRoomPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column,
+    );
+  }
+}
